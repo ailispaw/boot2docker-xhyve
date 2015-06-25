53aef27a87aa63e9f7de21163975fde35ddbc2a2
A.I <ailis@paw.zone>
2015-06-25 10:27:27 -0700

    Fix a bug with multiple tabs

--- xhyveexec.sh
+++ xhyveexec.sh
@@ -12,10 +12,10 @@ elif [ "${TERM_PROGRAM}" = "iTerm.app" ] ; then
   osascript <<END
     tell application "iTerm"
       tell application "System Events" to keystroke "d" using {shift down, command down}
-      tell application "System Events" to keystroke "[" using {command down}
-      tell the last session of current terminal
+      tell the current session of current terminal
         write text "${CMD}"
       end tell
+      tell application "System Events" to keystroke "[" using {command down}
     end tell
 END
 fi
