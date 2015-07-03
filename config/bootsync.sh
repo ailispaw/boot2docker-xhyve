c41876e29648e28dcfb2543428657e7b9e72918e
A.I <ailis@paw.zone>
2015-07-03 14:40:25 -0700

    Use a generic way to set the IP address

--- config/bootsync.sh
+++ config/bootsync.sh
@@ -3,4 +3,7 @@
 mkdir -p /Users
 umount /Users
 /usr/local/etc/init.d/nfs-client start
-mount 192.168.64.1:/Users /Users -o rw,async,noatime,rsize=32768,wsize=32768,proto=tcp
+GW_IP=$(ip route get 8.8.8.8 | awk 'NR==1 {print $3}')
+if [ -n "${GW_IP}" ]; then
+  mount ${GW_IP}:/Users /Users -o rw,async,noatime,rsize=32768,wsize=32768,proto=tcp
+fi
