77012a750a04763cd8450edaeb3c86ed38d0d8fd
A.I <ailis@paw.zone>
2018-11-07 19:47:04 -0800

    Fix an error on NFS mount

--- config/bootsync.sh
+++ config/bootsync.sh
@@ -5,5 +5,5 @@ umount /Users
 /usr/local/etc/init.d/nfs-client start
 GW_IP=$(ip route get 8.8.8.8 | awk 'NR==1 {print $3}')
 if [ -n "${GW_IP}" ]; then
-  mount ${GW_IP}:/Users /Users -o rw,async,noatime,rsize=32768,wsize=32768,proto=tcp
+  mount ${GW_IP}:/Users /Users -o vers=3,rw,async,noatime,rsize=32768,wsize=32768,proto=tcp
 fi
