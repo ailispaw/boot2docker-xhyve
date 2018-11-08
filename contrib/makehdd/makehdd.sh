2236ea6d3baaba062075b0234733e2810bdcacff
A.I <ailis@paw.zone>
2018-11-07 19:57:49 -0800

    Revise makehdd.sh for the latest boot2docker

--- contrib/makehdd/makehdd.sh
+++ contrib/makehdd/makehdd.sh
@@ -1,26 +1,6 @@
 #!/bin/sh
 
-LABEL=boot2docker-data
-
-DEV=/dev/vda
 MNT=/mnt/vda1
-
-(echo n; echo p; echo 2; echo; echo +1000M; echo w) | fdisk ${DEV}
-(echo t; echo 82; echo w) | fdisk ${DEV}
-until [ -b "${DEV}2" ]; do
-  sleep 0.5
-done
-mkswap ${DEV}2
-
-(echo n; echo p; echo 1; echo; echo; echo w) | fdisk ${DEV}
-until [ -b "${DEV}1" ]; do
-  sleep 0.5
-done
-mkfs.ext4 -L ${LABEL} ${DEV}1
-
-mkdir -p ${MNT}
-mount ${DEV}1 ${MNT}
-mkdir -p ${MNT}/var/lib/boot2docker
 
 curl -L https://raw.githubusercontent.com/ailispaw/boot2docker-xhyve/master/config/profile \
   -o ${MNT}/var/lib/boot2docker/profile
